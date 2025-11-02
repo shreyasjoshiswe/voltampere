ff85142fda5407b51ea41a00155d5d9930114073
Shreyas SWE <shreyas.joshi.swe@gmail.com>
2025-11-02 15:41:14 +0530

    feat(shared): branded types for simulation config

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -8,3 +8,22 @@ export type ChargingDemand = {
   readonly distanceKm: number;
 };
 
+// Really wish we'd have a Rust like unit struct,
+// something like `struct ChargePointID(u8)`,
+// instead of using branded types
+// but this works as well.
+export type ChargePointId = number & { readonly __brand: 'ChargepointId' };
+export type TimeSlot = number & { readonly __brand: 'TimeSlot' };
+export type Energy = number & { readonly __brand: 'Energy' };
+export type Power = number & { readonly __brand: 'Power' };
+export type ChargepointState =
+  | { tag: 'available' }
+  | { tag: 'charging'; remainingSlots: number; powerDraw: Power };
+
+export type SimulationConfig = {
+  readonly numChargepoints: number;
+  readonly arrivalMultiplier: number;
+  readonly carConsumption: number;
+  readonly chargingPower: number;
+};
+
